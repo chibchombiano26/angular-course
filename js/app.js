19aa4968c4afd95009476ff786180160da8fe835
Jose douglas ramirez <chibchombiano26@hotmail.com>
2015-11-18 22:52:00 +0000

    3

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 /*global angular*/
-var app = angular.module("myFirstApp",[])
+var app = angular.module("myFirstApp",['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+    
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('state1', {
+      url: "/state1",
+      templateUrl: "views/state1.html"
+    });
+})
